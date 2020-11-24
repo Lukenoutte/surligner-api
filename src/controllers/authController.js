19fb46484678129705c568ba737c140f0c5cbbe7
Lucas Lima <lucas.alima@souunit.com.br>
2020-11-24 18:35:15 -0300

    auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,24 +1,49 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.get("/", async function (req, res) {
- 
-});
+const authConfig = require("../config/auth");
+
+router.get("/", async function (req, res) {});
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 router.post("/register", async function (req, res) {
   const { email } = req.body;
   try {
-    if(await User.findOne({email}))
-      return res.status(400).send({ error: 'User already exists'});
+    if (await User.findOne({ email }))
+      return res.status(400).send({ error: "User already exists" });
 
     const user = await User.create(req.body);
     user.password = undefined;
 
-    return res.send({user})
+    return res.send({ user, token: generateToken({id: user.id}) });
   } catch (err) {
-       return res.status(400).send({ error: 'Resgistration falied'});
+    return res.status(400).send({ error: "Resgistration falied" });
   }
+});
+
+router.post("/authenticate", async function (req, res) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    res.status(400).send({ error: "User not found" });
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(400).send({ error: "Invalid password" });
+  }
+  user.password = undefined;
+
+  const token = res.send({ user, token: generateToken({id: user.id}) });
 });
 
 module.exports = router;
