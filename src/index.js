9950f8ee65fb8459910eb5674a4bc853da6d351c
Lucas Lima <lucas.alima@souunit.com.br>
2020-12-04 22:33:01 -0300

    front end login

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const authController = require('./app/controllers/authController');
 const projectController = require('./app/controllers/projectController');
+const cors = require('cors');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
+app.use(cors());
 app.use('/auth', authController);
 app.use('/projects', projectController);
+
 
 app.listen(3333);
