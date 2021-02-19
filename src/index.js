0549e69e669a7b8044b67d6edcc5dd6877a2acdb
Lucas Lima <lucas.alima@souunit.com.br>
2021-02-19 20:45:19 -0300

    https

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const authController = require('./app/controllers/authController');
 const projectController = require('./app/controllers/projectController');
 const cors = require('cors');
+const https = require('https');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -11,5 +12,13 @@ app.use(cors());
 app.use('/auth', authController);
 app.use('/projects', projectController);
 
-
+const httpsServer = https.createServer({
+    key: fs.readFileSync('/etc/ssl/certs/apache-selfsigned.crt'),
+    cert: fs.readFileSync('/etc/ssl/private/apache-selfsigned.key'),
+  }, app);
+  
 app.listen(3333);
+
+httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
